5a03ede8fca7c8a359fc758d62815fd4edc3cec8
Your Name <mibrahimtariq@icloud.com>
2025-04-14 16:06:44 +0100

    security fixes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  
-  // Disable the loading indicator that appears in the top-right corner
-  devIndicators: {
-    buildActivity: false,
-  },
   
   // Optimize image loading
   images: {
     unoptimized: false,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
   },
 };
 
